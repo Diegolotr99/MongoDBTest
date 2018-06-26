47a8e790e16117b07035a0c82a9bc89c1abb4366
Diego Gutierrez <diegolotr99@gmail.com>
2018-06-25 21:33:56 -0600

    added code to show how to iterate through documents in the collection and then get fields of that document.

--- src/ConnectToDB.java
+++ src/ConnectToDB.java
@@ -4,6 +4,8 @@ import com.mongodb.client.MongoDatabase;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.management.Query;
+
 import org.bson.Document;
 import com.mongodb.BasicDBObject;
 import com.mongodb.MongoClient;
@@ -27,7 +29,7 @@ public class ConnectToDB {
                                   .append("address", new BasicDBObject("street", "123 Fake St")
                                                                .append("city", "Faketon")
                                                                .append("state", "MA")
-                                                               .append("zip", 12345))
+                                                               	.append("zip", 12345))
                                   .append("books", books);
       
       
@@ -36,10 +38,18 @@ public class ConnectToDB {
       
       MongoCollection<Document> collection = database.getCollection("people"); //Get the Collection Person
           
-      collection.insertOne(person); //Insert the document object to that collection "people"
-
-      mongoClient.close(); // Close mongo connection just for this simple example. Since its not being used anymore.
+      //collection.insertOne(person); //Insert the document object to that collection "people"
      
+      System.out.println("Documents found: " + collection.count());
+      
+      for(Document d:collection.find()) { //Loop through all the documents found in the collection.
+    	  
+    	  System.out.println("Id: " + d.get("_id")); //Get the field named "_id" of the document
+    	  System.out.println("Name:" + d.get("name")); //Get the field named "name" of the document
+    	  
+      }
+     
+      mongoClient.close(); // Close mongo connection just for this simple example. Since its not being used anymore.
    } 
 
 }
